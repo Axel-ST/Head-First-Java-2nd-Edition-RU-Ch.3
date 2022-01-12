104d49ad25868bc30b72d53eca4e594836da8dfa
Axel_ST <Kozlov.axe@gmail.com>
2022-01-12 14:30:58 +0700

    Update Hobbits class files.

--- src/BeTheCompiler/Hobbits.java
+++ src/BeTheCompiler/Hobbits.java
@@ -7,5 +7,25 @@
 package BeTheCompiler;
 
 public class Hobbits {
-
+    String name;
+    
+    public static void main(String[] args) {
+        Hobbits[] h = new Hobbits[3];
+        int z = -1;
+        
+        while (z < 2) {
+            z += 1;
+            h[z] = new Hobbits();
+            h[z].name = "Bilbo";
+            
+            if (z == 1)
+                h[z].name = "Frodo";
+            
+            if (z == 2)
+                h[z].name = "Sam";
+            
+            System.out.print(h[z].name + " is ");
+            System.out.println("a name of a nice hobbit");
+        }
+    }
 }
